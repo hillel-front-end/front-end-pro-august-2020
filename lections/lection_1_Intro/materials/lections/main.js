3314e346191c47dd261f94c392c4cffef0af0f4b
sychovSaveliy <sychov.saveliy@gmail.com>
2020-08-28 21:36:46 +0200

    l-1

--- lections/lection_1_Intro/materials/lections/main.js
+++ lections/lection_1_Intro/materials/lections/main.js
@@ -18,3 +18,71 @@ Lection 1 - JavaScript Intro
      Этот подход применяется в браузерах для JavaScript. 
 */
 
+age = 1;
+firstName = 'Valera';
+lastName = "'Ternavskiy'";
+
+
+
+console.log(age, firstName, lastName);
+
+foo = 123;
+foo = 'Valera';
+
+
+
+a = 1; 
+b = 2;
+c = a + b;
+
+console.log(a + b, 'c');
+
+
+// g = c - 2;
+
+h = 11;
+z = 2;
+
+g = h * z;
+console.log(g, 'g = h * z');
+
+g = h / z;
+
+console.log(g, 'g = h / z;');
+
+g = h % z;
+
+console.log(g, '11 % 2');
+
+
+b = '123abc';
+
+console.log(b, 'b');
+
+
+sum = '12' + '13';
+
+age = 12;
+firstName = 'Valera';
+
+fullInfo = firstName  + age;
+
+
+console.log(fullInfo, 'firstName  + age');
+
+fullInfo = firstName  * age;
+
+
+console.log(fullInfo, 'firstName  * age');
+
+
+sum = '10' * '2';
+
+console.log(sum, 'sum');
+
+
+abc = '13 Valera';
+
+age = parseInt(abc);
+
+console.log(age, 'age');
