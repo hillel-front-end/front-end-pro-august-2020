5b93ab6cbae1deab3ae85f4734895660e804ed8b
ValeriyTernavskiygit config --global user.name ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2020-09-14 21:23:59 +0300

    l-6

--- lections/lection_5-6_functions+practice/materials/lections/main.js
+++ lections/lection_5-6_functions+practice/materials/lections/main.js
@@ -160,3 +160,31 @@ console.log(compareMaxSumArr([1,2,3],[10,12, 13], [4,5,6]));
 
 
 console.log(i, 'i');
+
+function foo() {
+   return 123;
+}
+
+a = foo();
+
+
+
+
+function getCentralElements() {
+   centralArray = [];
+
+   for(i = 0; i < arguments.length; i++) {
+      cutItems = getCutItems(arguments[i]);
+      centralArray = centralArray.concat(cutItems);
+   }
+
+   return centralArray;
+}
+
+function getCutItems(item) { // item = item
+   count = item.length % 2 == 0? 2 : 1;
+   position = Math.ceil(item.length / 2) - 1;
+   return item.splice(position, count);
+}
+
+console.log(getCentralElements([1, 2, 3, 4], [5, 6, 7, 8, 9], [14, 2, 1, 5, 6])); // [2, 3, 7, 1]
