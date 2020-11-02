f797668204be7fdcceab4b89ae0dbc9cdc2941be
Dk1775 <dmytro.karachan@gmail.com>
2020-11-02 21:17:07 +0200

    l-16

---
+++ lections/lection_16_es6_DOM(intro)/materials/dom.js
@@ -0,0 +1,97 @@
+/*
++ Глобальные обьекты Window, Document
++ JS обращение в html
++ html коллекция
++ html элемент
++ Обьекты classList, className, innerText
+*/
+
+// DOM - Document Object Model
+// Browser object model
+
+
+console.dir(document.body, 'document');//body
+console.dir(document.documentElement, 'html');
+
+const divs = document.getElementsByTagName('div');
+
+// console.log(divs[0].tagName, 'tagName');
+
+// console.log(divs, 'divs');
+
+
+// [].forEach.call(divs, (div) => console.log(div, 'div'));
+
+var classes = document.getElementsByClassName('item');
+
+// console.log(classes, 'classes');
+
+const id = document.getElementById('foo');
+
+// console.log(id.tagName, 'id');
+
+const element = document.querySelector('.parent div.item');
+const elements = document.querySelectorAll('div.item');
+
+console.dir(element, elements)
+
+// element.className += ' container';
+
+console.log(element.classList, 'element.classList'); // add, remove, toggle, replace
+
+// element.classList.add('accent');
+
+console.log(element.className, 'className');
+
+
+// setTimeout(() => {
+//     // element.align = 'center';
+//     element.classList.add('accent');
+// }, 2000);
+
+
+// setTimeout(() => {
+//     // element.align = 'center';
+//     element.classList.remove('accent');
+// }, 4000);
+
+
+
+
+// setInterval(() => {
+//     // element.align = 'center';
+//     element.classList.toggle('accent');
+// }, 1000);
+
+
+// ------ innerHTML, innerText -----------
+
+const goods = [
+    { name: 'juice', coast: 1, isBougth: false },
+    { name: 'cola', coast: 2, isBougth: true },
+    { name: 'sprite', coast: 3, isBougth: false },
+    { name: 'sprite', coast: 3, isBougth: true },
+    { name: 'sprite', coast: 30, isBougth: false }
+];
+
+const isRow = true;
+
+const markup = `
+<div class="goods-wrapper">
+    <ul class="goods ${isRow? 'row': 'column'}">
+        ${goods.map(({isBougth, name, coast}) => `<li class="goods__item ${isBougth? 'bought': ''}"> ${name}, ${coast}$ </li>`).join('')}
+    </ul>
+</div>
+`
+
+const markup2 = document.querySelector('.markup');
+
+console.dir(markup2, 'markupo'); 
+
+// markup2.innerHTML = markup;
+// markup2.innerText = markup;
+
+
+const input = document.querySelector('[type="text"]');
+
+console.dir(input.value = 'Hello front-end pro', 'input');
---
+++ lections/lection_16_es6_DOM(intro)/materials/main.js
@@ -0,0 +1,106 @@
+function getName(greeting) {
+    return `Valera ${greeting}`;
+}
+
+let str = `hello workd ${24 + 25} ${getName('Hello')}`;
+
+console.log(str, 'str');
+
+
+
+let person = "Valera" + (25 + 24);
+// let foo = "hello 
+// world";
+
+
+let foo = `Hello ${`${25 + 25} world`}`;
+const goods = [
+    { name: 'juice', coast: 1, isBougth: false },
+    { name: 'cola', coast: 2, isBougth: true },
+    { name: 'sprite', coast: 3, isBougth: false },
+    { name: 'sprite', coast: 3, isBougth: true },
+    { name: 'sprite', coast: 30, isBougth: false }
+];
+
+
+// console.log(goods.map(good => '<li>' + good.name + '</li>'))
+
+// const markup = 
+// '<div  class="goods-wrapper">'  
+//    + '<ul class="goods">'  
+//         + (goods.map(good => '<li class="goods__item">' + good.name + '</li>').join(''))
+//    + '</ul>'
+// +'</div>';
+
+
+const isRow = true;
+
+const markup = `
+<div class="goods-wrapper">
+    <ul class="goods ${isRow? 'row': 'column'}">
+        ${goods.map(({isBougth, name, coast}) => `<li class="goods__item ${isBougth? 'bought': ''}"> ${name}, ${coast}$ </li>`).join('')}
+    </ul>
+</div>
+`
+
+// console.log(markup, 'markup');
+
+document.write(markup);
+
+
+
+
+const product =  { 
+    name: 'sprite', 
+    coast: 30, 
+    isBougth: false,
+    childs: {
+        login: 'Aaa'
+    }
+
+};
+
+
+// let name = product.name;
+// let coast = product.coast;
+// let childs = product.childs.login;
+
+const {
+    name, 
+    isBougth, 
+    coast: c, 
+    childs: { login }
+} = product;
+
+
+
+
+
+
+// const c = product.coast;
+
+
+// console.log(name, isBougth, c, login);
+
+const array = [1, 2, 3, 4];
+
+// let a0 = array[0];
+
+const [a,, b , d] = array;
+
+// console.log(a,b , d, 'a, b, c , d');
+
+
+// func
+
+
+
+
+function foo2({name, coast}, zoo = []) {
+    console.log(name, coast);
+
+    // zoo = zoo || [];
+    console.log(zoo, 'zoo');
+}
+
+foo2({ name: 'sprite', coast: 30, isBougth: false }, [{name: 'lion'}])
